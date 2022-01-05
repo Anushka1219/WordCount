95d0b96054992cf055fa91bb44066069ea1fd55d
Anushka1219 <74186987+Anushka1219@users.noreply.github.com>
2022-01-05 18:05:03 +0530

    Add files via upload

---
+++ WordCount.py
@@ -0,0 +1,14 @@
+word=input("Enter the word :")
+
+count=0
+wordcount=1
+
+for i in word:
+           count=count+1
+           if(i==" "):
+               wordcount=wordcount+1
+    
+print(count)
+print(wordcount)
+
+
